a57cd8515a8492bb41d0bec4cb6c752061da6236
Ismail ARSLAN <arslan@Ismail-MacBook-Pro.local>
2026-07-20 21:46:20 +0300

    Add routes to serve favicon.png and og-image.png

--- server.js
+++ server.js
@@ -1393,6 +1393,16 @@ const server = http.createServer(async (req, res) => {
     return;
   }
 
+  if (req.method === 'GET' && req.url === '/favicon.png') {
+    res.writeHead(200, { 'Content-Type': 'image/png' });
+    res.end(fs.readFileSync(path.join(__dirname, 'favicon.png')));
+    return;
+  }
+  if (req.method === 'GET' && req.url === '/og-image.png') {
+    res.writeHead(200, { 'Content-Type': 'image/png' });
+    res.end(fs.readFileSync(path.join(__dirname, 'og-image.png')));
+    return;
+  }
   if (req.method === 'GET' && req.url === '/sitemap.xml') {
     res.writeHead(200, { 'Content-Type': 'application/xml' });
     res.end(fs.readFileSync(path.join(__dirname, 'sitemap.xml'), 'utf8'));
