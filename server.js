b3efdd56bfc7360dd4b8b0cca8c47a92d131a03b
Ismail ARSLAN <arslan@Ismail-MacBook-Pro.local>
2026-07-20 23:04:21 +0300

    Remove duplicate og-image route, shorten OG/Twitter descriptions

--- server.js
+++ server.js
@@ -1409,11 +1409,6 @@ const server = http.createServer(async (req, res) => {
     res.end(fs.readFileSync(path.join(__dirname, 'favicon.svg')));
     return;
   }
-  if (req.method === 'GET' && req.url === '/og-image.png') {
-    res.writeHead(200, { 'Content-Type': 'image/png' });
-    res.end(fs.readFileSync(path.join(__dirname, 'og-image.png')));
-    return;
-  }
   if (req.method === 'GET' && req.url === '/sitemap.xml') {
     res.writeHead(200, { 'Content-Type': 'application/xml' });
     res.end(fs.readFileSync(path.join(__dirname, 'sitemap.xml'), 'utf8'));
